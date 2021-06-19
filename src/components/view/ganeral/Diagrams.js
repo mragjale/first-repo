a17c1e2b036ddb397a290739c436615719103ca5
mragjale <ghanshyammragjale@gmail.com>
2021-06-19 11:31:53 +0530

    Some changes for tree.

--- src/components/view/ganeral/Diagrams.js
+++ src/components/view/ganeral/Diagrams.js
@@ -2,47 +2,94 @@ import React, { useCallback, useEffect, useState } from 'react'
 import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
 import 'beautiful-react-diagrams/styles.css';
 import '../../../styles/Contact.scss';
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const initialSchema = createSchema({
-  nodes: [
-    {
-      id: 'node-1',
-      content: 'Node 1',
-      coordinates: [150, 60],
-      outputs: [ { id: 'port-1', alignment: 'right' } ],
-    },
-  ]
-});
+// const initialSchema = createSchema({
+//   nodes: [
+//     {
+//       id: 'node-1',
+//       content: 'Node 1',
+//       coordinates: [150, 60],
+//       inputs: [
+//         { id: 'port-1', alignment: 'left' },
+//       ],
+//       outputs: [
+//         { id: 'port-2', alignment: 'right' },
+//       ],
 
-const CustomRender = ({ id, content, data, inputs, outputs }) => (
-    <div style={{background: 'purple'}}>
-      <div style={{textAlign: 'right'}}>
-        {/* <button  icon="times" size="small" onClick={()=>data.onClick(id)}/> */}
-        <button onClick={()=>data.onClick(id)}>
-      
-        close
-        </button> 
-      </div>
-      <div style={{padding: '15px'}}>
-        {content} dsfsdfsdf
-      </div>
-      <div style={{marginTop: '10px',display:'flex', justifyContent:'space-between'}}>
-        {inputs.map((port) => React.cloneElement(port, {style: { width: '25px', height: '25px', background: '#1B263B' }}))}
-        {outputs.map((port) => React.cloneElement(port, {style: { width: '25px', height: '25px', background: '#1B263B' }}))}
-      </div>
-    </div>
-);
+//     },
+//   ]
+// });
+
+
   const Diagrams = () => {
+  
   // create diagrams schema
-  const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
-  const  [label, setLabel] = useState('');
   const deleteNodeFromSchema = (id) => {
     const nodeToRemove = schema.nodes.find(node => node.id === id);
     removeNode(nodeToRemove);
   };
+
+  
+    const CustomRender = useCallback(({ id, content, data, inputs, outputs }) => (
+      <div style={{background: 'green'}}>
+        <div style={{textAlign: 'right'}}>
+          {/* <button  icon="times" size="small" onClick={()=>data.onClick(id)}/> */}
+          <button onClick={()=>data.onClick(id)}>
+        
+          close
+          </button> 
+        </div>
+        <div style={{padding: '15px'}}>
+          {content} 
+        </div>
+        <div style={{marginTop: '10px',display:'flex', justifyContent:'space-between'}}> <i className="fas fa-arrow-right"></i> <FontAwesomeIcon icon={faArrowRight} />
+          {inputs.map((port) => React.cloneElement(port, {style: { width: '25px', height: '25px', background: '#1B263B' },
+          className: "fas fa-arrow-right"}, ))}
+          {outputs.map((port) => React.cloneElement(port, {style: { width: '25px', height: '25px', background: '#1B263B' }}))}
+        </div>
+      </div>     
+  ));
+  const initSchema = JSON.parse(localStorage.getItem("schema"));
+  if (initSchema && initSchema.nodes) {
+    initSchema.nodes.forEach((node, key) => {
+      node.render = CustomRender;
+      node.data = {onClick: deleteNodeFromSchema}
+      initSchema.nodes[key] = node;
+    });
+  }
+  const initialSchema = createSchema(
+    initSchema
+      ? initSchema
+      : {
+          nodes: [
+    {
+      id: 'node-1',
+      content: 'Node 1',
+      coordinates: [150, 60],
+      inputs: [
+        { id: 'port-1', alignment: 'left' },
+      ],
+      outputs: [
+        { id: 'port-2', alignment: 'right' },
+      ],
+      data: {onClick: deleteNodeFromSchema}
+
+    },
+  ]
+        }
+  );
+  const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
+  const  [label, setLabel] = useState('');
+ 
 const saveSchema = () => {
   console.log('save schema', schema);
+  // schema.links.forEach(link => {
+  //   link.label = link.selected;
+  // });
+localStorage.setItem('schema', JSON.stringify(schema));
 }
   const addNewNode = useCallback (
     (e) =>  {
@@ -56,16 +103,19 @@ const saveSchema = () => {
        ],
        render: CustomRender,
        data: {onClick: deleteNodeFromSchema},
-       inputs: [{ id: `port-${Math.random()}`}],
+       inputs: [{ id: `port-${Math.random()}`, canLink: show}],
        outputs: [{ id: `port-${Math.random()}`}],
    };
    
    addNode(nextNode);
   }, [CustomRender, addNode, deleteNodeFromSchema, schema]
   );
+  function show() {
+    console.log('xvxvcxvxcvxcvxcvxcvxcvxvcxvxvxcv');
+  }
   const linkRender = () => (
     <div style={{background: 'purple'}}>
-  <select id="cars" name="cars" value={label} onChange={handleChange}>
+  <select id="cars" name="cars" onChange={handleChange}>
   <option value="volvo">Volvo</option>
   <option value="saab">Saab</option>
   <option value="fiat">Fiat</option>
@@ -82,15 +132,18 @@ console.log(e.target.value);
     if (schema && schema.links) {
       schema.links.forEach((link) => {
       
-    link.label = linkRender(label) 
-    link.selectedLabel = label;
+    link.label = linkRender()
+    link.selected = label;
       });
     }
     //return onChange(schema);
-  }, [schema]);
+  }, [schema, label]);
   useEffect(() => {
     handleSaveChange();
   });
+  // useEffect(() => {
+  //   setInitialSchema(JSON.parse(localStorage.getItem('schema')))
+  // }, []);
   return (
     <div style={{ height: '22.5rem' }}>
       <button   color="primary" icon="plus" onClick={addNewNode}>Add new node</button>
